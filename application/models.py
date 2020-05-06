31dd9d6d0a69934e525cd977d1992bf94084d4b1
LeoKnox <LeoKnox@users.noreply.github.com>
2020-05-05 22:29:28 -0700

    Add files via upload

--- application/models.py
+++ application/models.py
@@ -1,5 +1,6 @@
 import flask
 from application import db
+from werkzeug.security import generate_password_hash, check_password_hash
 
 class Builder(db.Document):
     builder_id  =   db.IntField( unique=True )
@@ -7,6 +8,12 @@ class Builder(db.Document):
     last_name   =   db.StringField( max_length=50 )
     email       =   db.StringField( max_length=30 )
     password    =   db.StringField( max_length=30 )
+
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
+    
+    def get_password(self, password):
+        return check_password_hash(self.password, password)
 
 class Room(db.Document):
     room_id     =   db.StringField( max_length=10, unique=True )
