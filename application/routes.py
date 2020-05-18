6f6c200e2d420438f2c666a040cad31386df90b3
LeoKnox <LeoKnox@users.noreply.github.com>
2020-05-18 13:17:43 -0700

    Add files via upload

--- application/routes.py
+++ application/routes.py
@@ -1,5 +1,13 @@
-from application import app
+from application import app, db
 from flask import render_template
+
+class Room(db.Document):
+    room_id     =   db.IntField( unique=True )
+    room_name   =   db.StringField( max_length =30 )
+    length      =   db.IntField()
+    width       =   db.IntField()
+    pos_x       =   db.IntField()
+    pos_y       =   db.IntField()
 
 @app.route("/")
 @app.route("/index")
--- config.py
+++ config.py
@@ -3,4 +3,4 @@ import os
 class Config(object):
     SECRET_KEY = os.envrion.get('SECRET_KEY') or "secret_string"
 
-    MONGODB_SETTINGS = { 'db' : 'UTA_Enrollment' }
+    MONGODB_SETTINGS = { 'db' : 'Flask_App' }
