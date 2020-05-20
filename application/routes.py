6b8db8aee6745afc42d4cb71bd7329889dd69870
LeoKnox <LeoKnox@users.noreply.github.com>
2020-05-19 17:10:44 -0700

    Add files via upload

--- application/routes.py
+++ application/routes.py
@@ -13,13 +13,22 @@ def dungeon():
     info = Room.objects.all()
     return render_template("dungeon.html", dungeon="active", info=info)
 
-@app.route("/room/")
+@app.route("/room/", methods=["GET", "POST"])
 @app.route("/room/<room_name>", methods=["GET", "POST"])
 def room(room_name="Entry"):
     if not room_name:
         room_name="Entry"
-    form = CreateRoomForm()
     room_data = Room.objects.get(room_name=room_name)
+    form = CreateRoomForm()
+    if form.validate_on_submit():
+        room_name   =   form.room_name.data
+        length      =   form.length.data
+        width       =   form.width.data
+        pos_x       =   form.pos_x.data
+        pos_y       =   form.pos_y.data
+    
+        room = Room(room_name=room_name,length=length,width=width,pos_x=pos_x,pos_y=pos_y)
+        room.save()
     return render_template("room.html", room="active", info=room_data, form=form)
 
 @app.route("/map")
