6a8d35c7aa4d27dda236af8fc8a1d7a0b3091752
LeoKnox <LeoKnox@users.noreply.github.com>
2020-05-21 21:40:45 -0700

    Add files via upload

--- application/static/js/index.js
+++ application/static/js/index.js
@@ -1,3 +1,8 @@
 function myFunction() {
-    document.getElementById("dots").innerHTML = "three";
+    document.getElementById("pi").innerHTML = "three ";
+    x = 5;
+    y = 5;
+    l = 5;
+    w = 5;
+    document.getElementById(x+"."+y).innerHTML = "X"
 }
