afcba07c7e7ed63f6d6ad37f5ad3ece39735e686
LeoKnox <LeoKnox@users.noreply.github.com>
2020-05-23 22:03:26 -0700

    Add files via upload

--- application/forms.py
+++ application/forms.py
@@ -1,6 +1,10 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, IntegerField
+from wtforms import StringField, SubmitField, IntegerField, FormField, FieldList
 from wtforms.validators import DataRequired, ValidationError
+
+class DoorForm(FlaskForm):
+    wall        =   IntegerField()
+    position    =   IntegerField()
 
 class CreateRoomForm(FlaskForm):
     room_name   =   StringField("Room Name", validators=[DataRequired()])
@@ -8,4 +12,5 @@ class CreateRoomForm(FlaskForm):
     width       =   IntegerField("Width", validators=[DataRequired()])
     pos_x       =   IntegerField("X Position", validators=[DataRequired()])
     pos_y       =   IntegerField("Y Position", validators=[DataRequired()])
+    doors       =   FieldList(FormField(DoorForm))
     submit      =   SubmitField("Submit")
