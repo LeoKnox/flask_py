60d55af9e4ce0f23c09b5eb3238ee29d7c830ee4
LeoKnox <LeoKnox@users.noreply.github.com>
2020-05-24 21:15:12 -0700

    Add files via upload

--- application/forms.py
+++ application/forms.py
@@ -3,8 +3,8 @@ from wtforms import StringField, SubmitField, IntegerField, FormField, FieldList
 from wtforms.validators import DataRequired, ValidationError
 
 class DoorForm(FlaskForm):
-    wall        =   IntegerField()
-    position    =   IntegerField()
+    wall        =   IntegerField("Wall")
+    position    =   IntegerField("Position")
 
 class CreateRoomForm(FlaskForm):
     room_name   =   StringField("Room Name", validators=[DataRequired()])
